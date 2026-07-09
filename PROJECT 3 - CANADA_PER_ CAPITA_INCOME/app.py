04054e97f380517e7a4f69cb27b7dc80495be4de
vedishasingh <singhvedisha2007@gmail.com>
2026-07-09 09:55:49 +0530

    Update app.py

--- PROJECT 3 - CANADA_PER_ CAPITA_INCOME/app.py
+++ PROJECT 3 - CANADA_PER_ CAPITA_INCOME/app.py
@@ -9,6 +9,8 @@ Original file is located at
 
 import streamlit as st
 import pandas as pd
+import numpy as np
+from pathlib import Path
 
 # ---------------------------------------
 # Page Configuration
